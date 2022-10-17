fed7655a653d16cebb07d32a9d5780a069ef08d6
daipeijia <dea9703@163.com>
2022-10-17 21:36:29 +0800

    引入antd样式

--- src/App.js
+++ src/App.js
@@ -1,14 +1,10 @@
 import './App.scss'
 import { BrowserRouter } from 'react-router-dom'
 import WrapperRoutes from './routes'
-import { Button } from 'antd'
-
 
 function App () {
   return (
     <div className="App">
-      <Button type='primary'>登录</Button>
-      <Button type='primary'>退出</Button>
       <BrowserRouter>
         <WrapperRoutes />
       </BrowserRouter>
