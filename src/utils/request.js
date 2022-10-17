9695016de7cafb08ab0ad9c924fb904160e0033a
daipeijia <dea9703@163.com>
2022-10-17 23:24:51 +0800

    统一注入token

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,4 +1,5 @@
 import axios from "axios"
+import { getToken } from './index'
 
 const request = axios.create({
   baseURL: 'http://geek.itheima.net/v1_0/',
@@ -8,6 +9,10 @@ const request = axios.create({
 // 添加请求拦截器
 request.interceptors.request.use(config => {
   // 如token统一注入
+  const token = getToken()
+  if (token) {
+    config.headers['Authorization'] = `Bearer${token}`
+  }
   return config
 }, error => {
   return Promise.reject(error)
@@ -17,7 +22,7 @@ request.interceptors.request.use(config => {
 request.interceptors.response.use(response => {
   // 2xx 范围内的状态码都会触发该函数。
   // 对响应数据做点什么
-  return response
+  return response.data
 }, error => {
   // 超出 2xx 范围的状态码都会触发该函数。
   // 对响应错误做点什么
