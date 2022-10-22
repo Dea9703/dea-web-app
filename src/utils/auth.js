4d93405d43cf10c7dc706d14034d159cd1e25eb0
daipeijia <dea9703@163.com>
2022-10-22 15:25:37 +0800

    更新Cookies存取token

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -2,7 +2,7 @@
 // token持久化：能够统一处理 token 的持久化相关操作
 
 // 实现步骤：
-// 1. 创建 utils/token.js 文件
+// 1. 创建 utils/auth.js 文件
 // 2. 分别提供 getToken/setToken/removeToken 三个工具函数并导出
 // 3. 创建 utils/index.js 文件，统一导出 token.js 中的所有内容，来简化工具函数的导入
 // 4. 将登录操作中用到 token 的地方，替换为该工具函数
@@ -12,11 +12,12 @@
 // 储存在 sessionStorage 中，每次调用接口时把它当为一个字段传给后台，浏览器关闭自动清除
 // 储存在 cookie 中，每次调用接口会自动发送，不过缺点是不能跨域
 
+import Cookies from "js-cookie"
 const TOKEN_KEY = 'dea-key'
 
-const getToken = () => localStorage.getItem(TOKEN_KEY)
-const setToken = (token) => localStorage.setItem(TOKEN_KEY, token)
-const removeToken = () => localStorage.removeItem(TOKEN_KEY)
+const getToken = () => Cookies.get(TOKEN_KEY)
+const setToken = (token) => Cookies.set(TOKEN_KEY, token)
+const removeToken = () => Cookies.remove(TOKEN_KEY)
 
 export {
   getToken,
--- src/utils/index.js
+++ src/utils/index.js
@@ -1,6 +1,6 @@
 // 先把所有工具函数导出的模块导入在这里，然后再统一导出
 import { request } from './request'
-import { getToken, setToken, removeToken } from './token'
+import { getToken, setToken, removeToken } from './auth'
 export {
   request,
   getToken,
